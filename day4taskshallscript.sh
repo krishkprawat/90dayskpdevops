4b217114d6fe091d13115f9746cff9854b282b69
krishnapal singh rawat <47602022+krishkprawat@users.noreply.github.com>
2023-04-22 22:40:01 +0530

    new sccript file added

---
+++ day4taskshallscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Take user input
+echo "What is your favorite color?"
+read color
+
+# Print out user input
+echo "Your favorite color is $color."
+
+# Take input from arguments
+echo "The first argument is: $1"
+echo "The second argument is: $2"
+echo "The third argument is: $3"
+
